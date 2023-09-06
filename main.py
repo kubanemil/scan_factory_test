5912b93d1f6eef6e65bf42f55196a4570194b0e2
Emil <emil3toktobekov@gmail.com>
2023-09-06 22:26:37 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
 import sqlite3
-from tools import is_trash_domain, get_regex
+from tools import Tools
 
-db_name = 'domains.db'
 
-with sqlite3.connect(db_name) as conn:
-    cursor = conn.cursor()
-    results = cursor.execute('SELECT * FROM domains;').fetchall()
-    column_names = [desc[0] for desc in cursor.description]
+class DBRegex(Tools):
+    def __init__(self, db_name='domains.db'):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.data = self.cursor.execute('SELECT * FROM domains;').fetchall()  # all rows from 'domains' table
+        self.create_rules()
 
-    domain_ids = set([row[0] for row in results])
+    def insert_into_rules_db(self, domain_id, regex):
+        """
+        Inserts data into rules (project_id, regexp).
+        """
+        results = self.cursor.execute(f'SELECT * FROM rules WHERE project_id={domain_id};').fetchall()
+        if len(results) == 0:
+            self.cursor.execute(f'INSERT INTO rules (project_id, regexp) VALUES ({domain_id}, "{regex}");')
+        else:
+            self.cursor.execute(f'UPDATE rules SET regexp="{regex}" where project_id={domain_id};')
+        self.conn.commit()
 
-    for domain_id in domain_ids:
-        domains = [row[1] for row in results if row[0] == domain_id]
+    def create_rules(self):
+        """
+        Creates Regular expressions for each project and inserts
+        it into 'rules' table.
+        """
+        domain_ids = set([row[0] for row in self.data])
 
-        regex = get_regex(domains)  # gets a regex pattern based on a given domains.
+        for domain_id in domain_ids:
+            domains = [row[1] for row in self.data if row[0] == domain_id]
 
-        insert_into_db(domain_id, regex)
-        cursor.execute(f'INSERT INTO rules (project_id, regexp) VALUES ({domain_id}, "{regex}")')
-    conn.commit()
+            regex = self.get_regex(domains)  # gets a regex pattern based on a given domains.
 
+            self.insert_into_rules_db(domain_id, regex)
+            self.conn.commit()
+
+    def filtered_domains(self):
+        """Returns 'domains' table but without 'trash' domains."""
+        filtered_rows = []
+        for row in self.data:
+            regex = self.cursor.execute(f'SELECT regexp FROM rules WHERE project_id={row[0]};').fetchone()[0]
+            if not self.is_trash_domain(row[1], regex):
+                filtered_rows.append(row)
+        return filtered_rows
+
+    def rules_table(self):
+        """
+        Returns 'rules' table that contains (project_id, regexp) columns from database.
+        """
+        return self.cursor.execute('SELECT * FROM rules;').fetchall()
+
+
+if __name__=='__main__':
+    instance = DBRegex()
+    print(instance.rules_table())
+    print(instance.filtered_domains())
--- tools.py
+++ tools.py
@@ -2,20 +2,20 @@ import re
 from statistics import mean
 
 
-def get_subdomain(domain):
-    return domain.split('.')[0].lower()
+class Tools:
+    @staticmethod
+    def get_subdomain(domain):
+        return domain.split('.')[0].lower()
+
+    def is_trash_domain(self, domain, regex):
+        subdomain = self.get_subdomain(domain)
+        return bool(re.match(regex, subdomain))
+
+    def get_regex(self, domains):
+        subdomains = [self.get_subdomain(domain) for domain in domains]
+        subdomains_len = [len(sub) for sub in subdomains]
+        meany, maxy = int(mean(subdomains_len)) - 1, max(subdomains_len)
+        return r'^.{%s,%s}$' % (meany, maxy)
 
 
-def is_trash_domain(domain, regex):
-    subdomain = get_subdomain(domain)
-    return bool(re.match(regex, subdomain))
 
-
-def get_regex(domains):
-    subdomains = [get_subdomain(domain) for domain in domains]
-    subdomains_len = [len(sub) for sub in subdomains]
-    meany, maxy = int(mean(subdomains_len)) - 1, max(subdomains_len)
-    return r'^.{%s,%s}$' % (meany, maxy)
-
-
-# def insert_into_db(domain_id, regex):
